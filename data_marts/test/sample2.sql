d4463532df13d93a33ac0d5649822231c072974a
k-0120 <67145026+k-0120@users.noreply.github.com>
2021-11-06 16:02:06 +0900

    sample2 upload

---
+++ data_marts/test/sample2.sql
@@ -0,0 +1,11 @@
+SELECT 
+  event_date 
+  , event_timestamp
+  , event_name
+  , key
+  , value.string_value
+  , value.int_value
+FROM 
+  `valid-progress-257914.analytics_254523623.events_20211105`
+  , UNNEST(event_params)
+;
