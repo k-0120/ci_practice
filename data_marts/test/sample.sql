582f199e53a1d180674fc281531da34108c3813c
komura.takayuki <koumura.takayuki0120@gmail.com>
2021-12-16 21:48:34 +0900

    test

--- data_marts/test/sample.sql
+++ data_marts/test/sample.sql
@@ -1,10 +1,9 @@
 SELECT 
-  event_name
-  , key
+     event_name
+    , key
   , value.string_value
-  , value.int_value
-FROM 
-  `valid-progress-257914.analytics_254523623.events_20211105`
+        , value.int_value
+FROM `valid-progress-257914.analytics_254523623.events_20211105`
   , UNNEST(event_params)
   
 ;
