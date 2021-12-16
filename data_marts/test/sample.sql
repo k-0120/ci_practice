f4a63e3e458dbc5b042b1011e3ba1c6207944464
k-0120 <k-0120@users.noreply.github.com>
2021-12-16 15:03:26 +0000

    Apply Prettier Change

--- data_marts/test/sample.sql
+++ data_marts/test/sample.sql
@@ -1,9 +1,9 @@
 SELECT
-     event_name,
+    event_name,
     key,
-  value.string_value,
-        value.int_value
+    value.string_value,
+    value.int_value
 FROM `valid-progress-257914.analytics_254523623.events_20211105`,
-  UNNEST(event_params)
+    UNNEST(event_params)
 
 ;
